441a57c934f050da70c5f15c11f166a4c3d3b2d0
hannanico <hannanicolasca@gmail.com>
2023-06-22 19:49:28 -0400

    Dark mode

--- public/scripts/first.js
+++ public/scripts/first.js
@@ -2,6 +2,8 @@
 //THERE ARE DIFFERENT SECTIONS OF CODE FOR THE DIFFERENT SECTIONS IN THE EJS
 //THE ONLY THINGS THAT CHANGE FROM SECTION TO SECTION IS THE NAME AND SOME NUMERIC VALUES
 
+const { MongoExpiredSessionError } = require("mongodb");
+
 function update() {
   //<-----------MAIN SECTION PART----------->//
   //Get where the information will be displayed
@@ -109,13 +111,27 @@ document.addEventListener("scroll", update);
 update();
 
 function darkMode() {
-  var about = document.getElementById("about");
-  about.classList.toggle("dark-mode");
-  var about_mode = document.getElementsByClassName("about_mode");
-  about_mode.classList.toggle("about_mode");
+  var about_mode = document.getElementById("about");
+  about_mode.classList.toggle("about_color");
+  about_mode.classList.toggle("dark-mode");
 
-  var skills = document.getElementById("skills");
-  skills.classList.toggle("dark-mode");
-  var skills_mode = document.getElementsByClassName("skills_mode");
-  skills_mode.classList.toggle("skills_mode");
+  var skills_mode = document.getElementById("skills");
+  skills_mode.classList.toggle("skills_color");
+  skills_mode.classList.toggle("dark-mode");
+
+  var resume_education_mode = document.getElementById("resume_education");
+  resume_education_mode.classList.toggle("resume_education_color");
+  resume_education_mode.classList.toggle("dark-mode");
+
+  var portfolio_mode = document.getElementById("portfolio");
+  portfolio_mode.classList.toggle("portfolio_color");
+  portfolio_mode.classList.toggle("dark-mode");
+
+  var contact_mode = document.getElementById("contact");
+  contact_mode.classList.toggle("contact_color");
+  contact_mode.classList.toggle("dark-mode");
+
+  var error_mode = document.getElementById("error");
+  error_mode.classList.toggle("error_color");
+  error_mode.classList.toggle("dark-mode");
 }
